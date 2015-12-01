38662e920999797ff2702ca5e4ef0951c3bd5d9f
Waylan Punch <waylanpunch@gmail.com>
2015-12-02 03:59:28 +0800

    Framework

---
+++ app/src/main/java/com/way/tunnelvision/ui/DiaryFragment.java
@@ -0,0 +1,33 @@
+package com.way.tunnelvision.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.way.tunnelvision.R;
+
+/**
+ * Created by pc on 2015/11/29.
+ */
+public class DiaryFragment extends Fragment {
+    private final static String TAG = DiaryFragment.class.getName();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView debug, start");
+        Log.d(TAG, "onCreateView debug, end");
+        return inflater.inflate(R.layout.content_diary, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated debug, start");
+        Log.d(TAG, "onActivityCreated debug, end");
+    }
+}
